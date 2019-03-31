059ff8d15d9b16d95b3fe81c617bc8ca40c82d15
gaoganghua <gaoganghua@gxb.io>
2019-03-31 20:20:14 +0800

    init config

--- registry-center/src/main/java/com/code/center/ServiceCenterApplication.java
+++ registry-center/src/main/java/com/code/center/ServiceCenterApplication.java
@@ -2,10 +2,12 @@ package com.code.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class ServiceCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCenterApplication.class, args);
