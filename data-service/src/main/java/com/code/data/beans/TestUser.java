93f020366ce446817d6994f18be1c873eb6bced1
gaoganghua <gaoganghua@gxb.io>
2019-05-29 17:45:58 +0800

    添加mybatisgenerator

---
+++ data-service/src/main/java/com/code/data/beans/TestUser.java
@@ -0,0 +1,11 @@
+package com.code.data.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestUser {
+    private Integer id;
+    private String name;
+}
