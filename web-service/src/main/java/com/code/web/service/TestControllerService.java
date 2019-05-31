f4339b91d6a14ce93f6a369f9577c42b520cfc5a
gaoganghua <gaoganghua@gxb.io>
2019-05-31 21:39:18 +0800

    添加web

--- web-service/src/main/java/com/code/web/service/TestControllerService.java
+++ web-service/src/main/java/com/code/web/service/TestControllerService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +30,16 @@ public class TestControllerService {
         return "index";
     }
 
+    @RequestMapping("/test")
+    public String test(@RequestParam(value = "msg", required = false) String msg) {
+        if (StringUtils.isNotEmpty(msg)) {
+            throw new CodecException("系统异常");
+        }
+        return "test";
+    }
+
     @RequestMapping("/newList")
-    public String queryNewsList(@RequestParam("modules") String modules, @RequestParam("type") String type) {
+    public ModelAndView queryNewsList(@RequestParam("modules") String modules, @RequestParam("type") String type) {
 
         List<News> newsList = new ArrayList<>();
         ResponseEntity<String> responseEntity = remoteRestTemplate.getForEntity("http://localhost:8081/news/queryNewsList?modulesMsg=" + modules + "&newsMsg=" + type, String.class);
@@ -38,7 +47,8 @@ public class TestControllerService {
         ResponseData responseData = JSON.parseObject(responseEntity.getBody(), ResponseData.class);
         newsList = JSON.parseArray((String) responseData.getResultData(), News.class);
         System.out.println(JSON.toJSONString(newsList));
-        return "index";
+
+        return new ModelAndView("index", "newList", newsList);
     }
 
 }
